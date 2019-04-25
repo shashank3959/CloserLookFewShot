a304ccf943d57b106c77221d7c089042cebc8e7a
Shashank Verma <shashank3959@gmail.com>
2019-04-24 23:43:50 -0500

    Change save directory

--- configs.py
+++ configs.py
@@ -1,4 +1,5 @@
-save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+#save_dir                    = '/work/newriver/wyharveychen/CloserLookFewShot/'
+save_dir                    = '/home/shashankv/Study/ECE699/CloserLookFewShot/'
 data_dir = {}
 data_dir['CUB']             = './filelists/CUB/' 
 data_dir['miniImagenet']    = './filelists/miniImagenet/' 
